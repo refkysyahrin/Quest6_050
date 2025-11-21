4d0a6ec99488c2d3755956084ca03e4447a6146c
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2025-11-21 15:15:16 +0700

    Menambahkan konstanta untuk jenis kelamin

--- app/src/main/java/com/example/arsitekturmvvm_050/model/DataJK.kt
+++ app/src/main/java/com/example/arsitekturmvvm_050/model/DataJK.kt
@@ -1,4 +1,10 @@
 package com.example.arsitekturmvvm_050.model
 
+import com.example.arsitekturmvvm_050.R
+
 object DataJK {
+    val JenisK= listOf(
+        R.string.laki,
+        R.string.perempuan
+    )
 }
